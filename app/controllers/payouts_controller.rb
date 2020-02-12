d9ce263fbe66cac57dc85cee8237dd88af01364d
Peter M Adrianov <peter.adrianov@gmail.com>
2020-02-13 01:20:09 +0300

    Create Payout form.

---
+++ app/controllers/payouts_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+class PayoutsController < ApplicationController
+  # GET /payouts
+  def index
+    @payouts = Payout.all
+  end
+
+  # GET /payouts/1
+  def show
+    @payout = Payout.find(params[:id])
+  end
+
+  # GET /payouts/new
+  def new
+    @payout = Payout.new
+  end
+
+  # POST /payouts
+  def create
+    @payout = Payout.new(payout_params)
+
+    if @payout.save
+      redirect_to @payout, notice: 'Создан запрос на выплату.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def payout_params
+    params.require(:payout).permit(:amount, :phone, :client, :receiver_fio)
+  end
+end
--- app/models/payout.rb
+++ app/models/payout.rb
@@ -1,2 +1,9 @@
+# Запрос на выплату
 class Payout < ApplicationRecord
+  before_validation(on: :create) do
+    self.dt = DateTime.now unless attribute_present?(:dt)
+
+    # направление выплаты: банковская карта
+    self.destination = 1 unless attribute_present?(:destination)
+  end
 end
