10eabd40baed1dd665809670705c6fd44f62a8f6
Peter M Adrianov <peter.adrianov@gmail.com>
2020-02-13 11:07:45 +0300

    Add basic validations.

--- app/models/payout.rb
+++ app/models/payout.rb
@@ -6,4 +6,12 @@ class Payout < ApplicationRecord
     # направление выплаты: банковская карта
     self.destination = 1 unless attribute_present?(:destination)
   end
+
+  validates :amount, numericality: { greater_than: 0 }, presence: true
+  validates :phone, length: 10..12, presence: true
+
+  # номер банковской карты
+  validates :client, length: 16..20, presence: true
+
+  validates :receiver_fio, presence: true
 end
